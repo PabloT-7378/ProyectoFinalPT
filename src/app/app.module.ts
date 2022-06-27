95e1f97d63a7215c9accde0c420909d75ad8af25
PabloT-7378 <nellyantonio7378@gmail.com>
2022-06-27 16:09:52 -0300

    banner finalizado, comensando con acerca de mi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BarradeinicioComponent } from './components/barradeinicio/barradeinicio.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BarradeinicioComponent
+    BarradeinicioComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,

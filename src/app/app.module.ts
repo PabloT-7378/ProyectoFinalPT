2221292a482b6a39f51aa8c5bb42729eb1e8c690
Pablo <92050907+PabloT-7378@users.noreply.github.com>
2022-06-30 08:26:39 -0300

    modelado de experiencia, seguimos con eucacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { BarradeinicioComponent } from './components/barradeinicio/barradeinicio.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercademiComponent } from './components/acercademi/acercademi.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AcercademiComponent } from './components/acercademi/acercademi.componen
     HeaderComponent,
     BarradeinicioComponent,
     BannerComponent,
-    AcercademiComponent
+    AcercademiComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,

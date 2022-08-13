7bfaeffdd70daeb29bc5128eef0165c9cc989608
PabloT-7378 <nellyantonio7378@gmail.com>
2022-08-13 07:58:07 -0300

    arreglos

--- Back-End-SpringBoot/pablot/src/main/java/com/portfoliopat/pablot/PablotApplication.java
+++ Back-End-SpringBoot/pablot/src/main/java/com/portfoliopat/pablot/PablotApplication.java
@@ -1,4 +1,4 @@
-package com.miproyectofinal.pablot;
+package com.portfoliopat.pablot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Back-End-SpringBoot/pablot/src/test/java/com/portfoliopat/pablot/PablotApplicationTests.java
+++ Back-End-SpringBoot/pablot/src/test/java/com/portfoliopat/pablot/PablotApplicationTests.java
@@ -1,4 +1,4 @@
-package com.miproyectofinal.pablot;
+package com.portfoliopat.pablot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
